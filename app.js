6ed0b532cbd0db344d274e1ef225c3b2a9956b00
rohilpatel15 <rohilpatel123@outlook.com>
2024-02-18 22:53:11 -0800

    fixing bugs

--- app.js
+++ app.js
@@ -59,4 +59,4 @@ http.createServer(function(req, res) {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('Page not found');
     }
-}). listen(3000);
+}). listen(PROCCESS.ENV.PORT || 3000);
